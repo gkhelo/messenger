c3d8767b12b2a7a81244adb0dafd323b2757a201
Broxigar <58048604+d-chechelashvili@users.noreply.github.com>
2021-08-09 17:25:32 +0400

    added bottom navigation app bar visual

--- app/src/main/java/ge/gkhelashvili/messenger/main/MainActivity.kt
+++ app/src/main/java/ge/gkhelashvili/messenger/main/MainActivity.kt
@@ -3,6 +3,7 @@ package ge.gkhelashvili.messenger.main
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ge.gkhelashvili.messenger.R
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.i(TAG, "Created activity")
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.background = null
+        bottomNav.menu.getItem(1).isEnabled = false
     }
 
     companion object {
